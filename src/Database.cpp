645842553e2af7fce09dfbed32d5c2f3c7875449
Shayden seymour <shaydenseymour@gmail.com>
2025-05-21 12:11:26 +1200

    Improve and cleanup database code

--- src/Database.cpp
+++ src/Database.cpp
@@ -5,12 +5,13 @@
 #include <ios>
 #include <iostream>
 #include <sstream>
+#include "Main.h"
 
-static std::stringstream openCertificate(const std::string path) {
+static std::stringstream openCertificate(const std::string& path) {
   std::ifstream certificateStream(path, std::ios::in);
-  if (!certificateStream.is_open() || !certificateStream.good()) {
+  if (!certificateStream.is_open()) {
     std::cerr << "Failed to open " << path << "\n";
-    exit(EXIT_FAILURE);
+    shutdown(EXIT_FAILURE);
   }
 
   std::stringstream file;
@@ -18,49 +19,54 @@ static std::stringstream openCertificate(const std::string path) {
   return std::move(file);
 }
 
-void Database::connect() {
-  // Use SSL
-  std::stringstream publicCert = openCertificate("data/database-cert.pem");
-  std::stringstream privateKey = openCertificate("data/database-key.key");
-  std::stringstream rootca = openCertificate("data/database-rootca.pem");
+void Database::setupSSL() {
+  const std::stringstream publicCert = openCertificate("data/database-cert.pem");
+  const std::stringstream privateKey = openCertificate("data/database-key.key");
+  const std::stringstream rootca = openCertificate("data/database-rootca.pem");
 
-  CassError addPublicCertError = cass_ssl_set_cert(Database::ssl.get(), publicCert.str().c_str());
+  const CassError addPublicCertError = cass_ssl_set_cert(Database::ssl.get(), publicCert.str().c_str());
   if (addPublicCertError != CASS_OK) {
     std::cerr << "Failed to set public key: " << cass_error_desc(addPublicCertError) << std::endl;
-    exit(EXIT_FAILURE);
+    shutdown(EXIT_FAILURE);
   }
 
-  CassError addPrivateKeyError = cass_ssl_set_private_key(Database::ssl.get(), privateKey.str().c_str(), nullptr);
+  const CassError addPrivateKeyError = cass_ssl_set_private_key(ssl.get(), privateKey.str().c_str(), nullptr);
   if (addPrivateKeyError != CASS_OK) {
     std::cerr << "Failed to set private key: " << cass_error_desc(addPrivateKeyError) << std::endl;
-    exit(EXIT_FAILURE);
+    shutdown(EXIT_FAILURE);
   }
 
-  CassError addRootcaError = cass_ssl_add_trusted_cert(Database::ssl.get(), rootca.str().c_str());
+  const CassError addRootcaError = cass_ssl_add_trusted_cert(ssl.get(), rootca.str().c_str());
   if (addRootcaError != CASS_OK) {
     std::cerr << "Failed to set rootca key: " << cass_error_desc(addRootcaError) << std::endl;
-    exit(EXIT_FAILURE);
+    shutdown(EXIT_FAILURE);
   }
 
-  // Set options now
-  cass_cluster_set_ssl(Database::cluster.get(), Database::ssl.get());
-  cass_cluster_set_credentials(Database::cluster.get(), "username", "password");
-  cass_cluster_set_connect_timeout(Database::cluster.get(), 10000);
-  cass_cluster_set_request_timeout(Database::cluster.get(), 5000);
-  cass_cluster_set_connection_idle_timeout(Database::cluster.get(), 60);
-  cass_cluster_set_load_balance_round_robin(Database::cluster.get());
-  cass_cluster_set_constant_reconnect(Database::cluster.get(), 3000);
-  cass_cluster_set_retry_policy(Database::cluster.get(), retryPolicy.get());
-  cass_cluster_set_port(Database::cluster.get(), 6969);
+  cass_cluster_set_ssl(cluster.get(), ssl.get());
+}
 
-  CassError setHostsError = cass_cluster_set_contact_points(Database::cluster.get(), "db.buildinger.org");
+void Database::configureOptions() {
+  cass_cluster_set_credentials(cluster.get(), username, password);
+  cass_cluster_set_connect_timeout(cluster.get(), 10000);
+  cass_cluster_set_request_timeout(cluster.get(), 5000);
+  cass_cluster_set_connection_idle_timeout(cluster.get(), 60);
+  cass_cluster_set_load_balance_round_robin(cluster.get());
+  cass_cluster_set_constant_reconnect(cluster.get(), 3000);
+  cass_cluster_set_retry_policy(cluster.get(), retryPolicy.get());
+  cass_cluster_set_port(cluster.get(), port);
+
+  const CassError setHostsError = cass_cluster_set_contact_points(cluster.get(), contactPoints);
   if (setHostsError != CASS_OK) {
     std::cerr << "Failed to set hosts: " << cass_error_desc(setHostsError) << std::endl;
     exit(EXIT_FAILURE);
   }
-  
-  // Connect
-  std::unique_ptr<CassFuture, void(*)(CassFuture*)> connectKeyspace(cass_session_connect_keyspace(Database::connection.get(), Database::cluster.get(), "bc"), cass_future_free);
+}
+
+void Database::connect() {
+  setupSSL();
+  configureOptions();
+
+  const std::unique_ptr<CassFuture, void(*)(CassFuture*)> connectKeyspace(cass_session_connect_keyspace(Database::connection.get(), Database::cluster.get(), "bc"), cass_future_free);
   if (cass_future_error_code(connectKeyspace.get()) != CASS_OK) {
     const char* errorMessage;
     size_t errorMessageSize;
@@ -79,16 +85,17 @@ void Database::disconnect(CassSession* connection) {
     return;
   }
 
-  std::unique_ptr<CassFuture, void(*)(CassFuture*)> closeFuture(cass_session_close(connection), cass_future_free);
+  const std::unique_ptr<CassFuture, void(*)(CassFuture*)> closeFuture(cass_session_close(connection), cass_future_free);
 
-  // Don't think we need a callback for this, doesn't really matter anyways
+  // Don't think we need a callback for this, doesn't really matter anyway
   cass_future_wait(closeFuture.get());
   if (cass_future_error_code(closeFuture.get()) != CASS_OK) {
     const char* message;
     size_t messageSize;
     cass_future_error_message(closeFuture.get(), &message, &messageSize);
-    std::cerr << "Failed to disconnect from database: " << std::string(message, messageSize) << std::endl << "\n";
+    std::cerr << "Failed to disconnect from database: " << std::string(message, messageSize) << std::endl;
   }
 
   cass_session_free(connection);
+  connection = nullptr;
 }
--- src/include/Database.h
+++ src/include/Database.h
@@ -8,6 +8,17 @@ namespace Database {
   void disconnect();
   void disconnect(CassSession* connection); // Overload for connection unique_ptr
 
+  void setupSSL();
+  void configureOptions();
+
+  // Network connections options
+  constexpr const char* contactPoints = "db.buildinger.org";
+  constexpr int port = 6969;
+
+  // I would usually use a .env
+  constexpr const char* username = "bc-h9u0XvYWuDFmFUQr4mqH3mzpM16ujP";
+  constexpr const char* password = "h9u0XvYWuDFmFUQr4mqH3mzpM16ujPh9u0XvYWuDFmFUQr4mqH3mzpM16ujP";
+
   inline std::unique_ptr<CassSession, void(*)(CassSession*)> connection(cass_session_new(), Database::disconnect);
   inline std::unique_ptr<CassCluster, void(*)(CassCluster*)> cluster(cass_cluster_new(), cass_cluster_free);
   inline std::unique_ptr<CassSsl, void(*)(CassSsl*)> ssl(cass_ssl_new(), cass_ssl_free);
