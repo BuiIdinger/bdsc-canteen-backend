17ce829aa93dcfd239eeb55f85eb05acd696c115
Shayden seymour <shaydenseymour@gmail.com>
2025-05-21 12:14:45 +1200

    Shutdown function

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,10 @@
 #include "WebSocket.h"
 #include <bwss/bwss.h>
 #include "Database.h"
+#include "Main.h"
+void shutdown(const int& code) noexcept {
+  exit(code);
+}
 
 int main() {
   // Database::connect();
