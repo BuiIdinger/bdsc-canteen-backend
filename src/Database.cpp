25ec7ed3e8261e769cc6f77abe5aaaea9ee4a4a7
Shayden seymour <shaydenseymour@gmail.com>
2025-05-23 18:27:33 +1200

    Improve database code

--- src/Database.cpp
+++ src/Database.cpp
@@ -3,20 +3,78 @@
 #include <memory>
 #include <fstream>
 #include <ios>
-#include <iostream>
 #include <sstream>
-#include "Main.h"
+#include "Log.h"
 
+/**
+ * Used for SSL certificates when connecting
+ */
 static std::stringstream openCertificate(const std::string& path) {
   std::ifstream certificateStream(path, std::ios::in);
   if (!certificateStream.is_open()) {
-    std::cerr << "Failed to open " << path << "\n";
-    shutdown(EXIT_FAILURE);
+    Log::critical("Failed to open file:: " + path);
+    UNREACHABLE;
   }
 
   std::stringstream file;
   file << certificateStream.rdbuf();
   return std::move(file);
+}
+
+/**
+ * Local cluster functions
+ */
+void Database::LocalCluster::setupOptions() {
+  // Set timeouts
+  cass_cluster_set_connect_timeout(cluster.get(), connectTimeout);
+  cass_cluster_set_request_timeout(cluster.get(), requestTimeout);
+  cass_cluster_set_resolve_timeout(cluster.get(), dnsResolveTimeout);
+  cass_cluster_set_connection_idle_timeout(cluster.get(), connectionIdleTimeout);
+  cass_cluster_set_constant_reconnect(cluster.get(), reconnectionAttemptTimeout);
+
+  // Other options
+  cass_cluster_set_load_balance_round_robin(cluster.get());
+  cass_cluster_set_retry_policy(cluster.get(), retryPolicy.get());
+
+  const CassError setPortError = cass_cluster_set_port(cluster.get(), port);
+  if (setPortError != CASS_OK) {
+    Log::Database::critical("Failed to set port: " + std::string(cass_error_desc(setPortError)));
+    UNREACHABLE;
+  }
+
+  const CassError setHostsError = cass_cluster_set_contact_points(cluster.get(), contactPoints);
+  if (setHostsError != CASS_OK) {
+    Log::Database::critical("Failed to set hosts: " + std::string(cass_error_desc(setHostsError)));
+    UNREACHABLE;
+  }
+}
+
+/**
+ * Remote cluster functions
+ */
+void Database::RemoteCluster::setupOptions() {
+  // Set timeouts
+  cass_cluster_set_connect_timeout(cluster.get(), connectTimeout);
+  cass_cluster_set_request_timeout(cluster.get(), requestTimeout);
+  cass_cluster_set_connection_idle_timeout(cluster.get(), connectionIdleTimeout);
+  cass_cluster_set_constant_reconnect(cluster.get(), reconnectionAttemptTimeout);
+
+  // Other options
+  cass_cluster_set_load_balance_round_robin(cluster.get());
+  cass_cluster_set_retry_policy(cluster.get(), retryPolicy.get());
+  cass_cluster_set_credentials(cluster.get(), username, password);
+
+  const CassError setPortError = cass_cluster_set_port(cluster.get(), port);
+  if (setPortError != CASS_OK) {
+    Log::Database::critical("Failed to set port: " + std::string(cass_error_desc(setPortError)));
+    UNREACHABLE;
+  }
+
+  const CassError setHostsError = cass_cluster_set_contact_points(cluster.get(), contactPoints);
+  if (setHostsError != CASS_OK) {
+    Log::Database::critical("failed to set hosts: " + std::string(cass_error_desc(setHostsError)));
+    UNREACHABLE;
+  }
 }
 
 void Database::setupSSL() {
@@ -26,56 +84,52 @@ void Database::setupSSL() {
 
   const CassError addPublicCertError = cass_ssl_set_cert(Database::ssl.get(), publicCert.str().c_str());
   if (addPublicCertError != CASS_OK) {
-    std::cerr << "Failed to set public key: " << cass_error_desc(addPublicCertError) << std::endl;
-    shutdown(EXIT_FAILURE);
+    Log::Database::critical("Failed to set public key: " + std::string(cass_error_desc(addPublicCertError)));
+    UNREACHABLE;
   }
 
   const CassError addPrivateKeyError = cass_ssl_set_private_key(ssl.get(), privateKey.str().c_str(), nullptr);
   if (addPrivateKeyError != CASS_OK) {
-    std::cerr << "Failed to set private key: " << cass_error_desc(addPrivateKeyError) << std::endl;
-    shutdown(EXIT_FAILURE);
+    Log::Database::critical("Failed to set private key: " + std::string(cass_error_desc(addPrivateKeyError)));
+    UNREACHABLE;
   }
 
   const CassError addRootcaError = cass_ssl_add_trusted_cert(ssl.get(), rootca.str().c_str());
   if (addRootcaError != CASS_OK) {
-    std::cerr << "Failed to set rootca key: " << cass_error_desc(addRootcaError) << std::endl;
-    shutdown(EXIT_FAILURE);
+    Log::Database::critical("Failed to set RootCA: " + std::string(cass_error_desc(addRootcaError)));
+    UNREACHABLE;
   }
 
   cass_cluster_set_ssl(cluster.get(), ssl.get());
 }
 
-void Database::configureOptions() {
-  cass_cluster_set_credentials(cluster.get(), username, password);
-  cass_cluster_set_connect_timeout(cluster.get(), 10000);
-  cass_cluster_set_request_timeout(cluster.get(), 5000);
-  cass_cluster_set_connection_idle_timeout(cluster.get(), 60);
-  cass_cluster_set_load_balance_round_robin(cluster.get());
-  cass_cluster_set_constant_reconnect(cluster.get(), 3000);
-  cass_cluster_set_retry_policy(cluster.get(), retryPolicy.get());
-  cass_cluster_set_port(cluster.get(), port);
-
-  const CassError setHostsError = cass_cluster_set_contact_points(cluster.get(), contactPoints);
-  if (setHostsError != CASS_OK) {
-    std::cerr << "Failed to set hosts: " << cass_error_desc(setHostsError) << std::endl;
-    exit(EXIT_FAILURE);
-  }
-}
-
 void Database::connect() {
+  Log::Database::info("Connecting to database");
+
+#ifdef DB_CLUSTER_LOCAL
+  LocalCluster::setupOptions();
+#endif
+
+#ifdef DB_CLUSTER_REMOTE
+  RemoteCluster::setupOptions();
   setupSSL();
-  configureOptions();
+#endif
 
   const std::unique_ptr<CassFuture, void(*)(CassFuture*)> connectKeyspace(cass_session_connect_keyspace(Database::connection.get(), Database::cluster.get(), "bc"), cass_future_free);
   if (cass_future_error_code(connectKeyspace.get()) != CASS_OK) {
     const char* errorMessage;
     size_t errorMessageSize;
     cass_future_error_message(connectKeyspace.get(), &errorMessage, &errorMessageSize);
-    std::cerr << "Failed to connect: " << std::string(errorMessage, errorMessageSize) << std::endl;
-    exit(EXIT_FAILURE);
+    Log::Database::critical("Failed to connect to cluster: " + std::string(errorMessage, errorMessageSize));
+    UNREACHABLE;
   }
+
+  Log::Database::info("Connected to database");
 }
 
+/**
+ * Disconnect functions
+ */
 void Database::disconnect() {
   disconnect(connection.get());
 }
@@ -93,9 +147,26 @@ void Database::disconnect(CassSession* connection) {
     const char* message;
     size_t messageSize;
     cass_future_error_message(closeFuture.get(), &message, &messageSize);
-    std::cerr << "Failed to disconnect from database: " << std::string(message, messageSize) << std::endl;
   }
 
   cass_session_free(connection);
   connection = nullptr;
+}
+
+void Database::prepareStatement(const std::unique_ptr<const CassPrepared, void(*)(const CassPrepared*)>& statement, const std::string& query) {
+
+  /*
+  std::unique_ptr<CassFuture, void(*)(CassFuture*)> future(cass_session_prepare(Database::connection.get(), query.c_str()), cass_future_free);
+  cass_future_wait(future.get());
+  if (cass_future_error_code(future.get()) != CASS_OK) {
+    const char* errorMessage;
+    size_t errorMessageSize;
+    cass_future_error_message(future.get(), &errorMessage, &errorMessageSize);
+    return std::unique_ptr<const CassPrepared, void(*)(const CassPrepared*)>(nullptr, cass_prepared_free);
+  }
+
+  const CassPrepared* prepared = cass_future_get_prepared(future.get());
+
+  return std::unique_ptr<const CassPrepared, void(*)(const CassPrepared*)>(prepared, cass_prepared_free);
+  cass_prepared_free(prepared); */
 }
--- src/include/Database.h
+++ src/include/Database.h
@@ -3,21 +3,60 @@
 #include <cassandra.h>
 #include <memory>
 
+#define DB_CLUSTER_LOCAL
+
 namespace Database {
+  /**
+   * Keyspace to connect to, this avoids having to explicit
+   * set the keyspace in every query
+   */
+  constexpr const char* connectKeyspace = "bc";
+
+  constexpr const char* username = "bc-h9u0XvYWuDFmFUQr4mqH3mzpM16ujP";
+  constexpr const char* password = "h9u0XvYWuDFmFUQr4mqH3mzpM16ujPh9u0XvYWuDFmFUQr4mqH3mzpM16ujP";
+
+  /**
+   * Timeout options
+   * All in MS time
+   */
+  constexpr int connectTimeout = 10000;
+  constexpr int requestTimeout = 5000;
+  constexpr int dnsResolveTimeout = 3000;
+  constexpr int connectionIdleTimeout = 60; // In seconds not MS
+
+  // Amount of milliseconds before a reconnection is attempted
+  constexpr int reconnectionAttemptTimeout = 0;
+
+  /**
+   * Setups clusters SSL, currently only used for connecting
+   * to the remote cluster
+   */
+  void setupSSL();
+
+  namespace LocalCluster {
+    // Network connections options
+    constexpr const char* contactPoints = "127.0.0.1";
+    constexpr int port = 9042;
+
+    void setupOptions();
+  }
+
+  namespace RemoteCluster {
+    // Network connections options
+    constexpr const char* contactPoints = "db-prod-canteen.buildinger.org";
+    constexpr int port = 9042;
+
+    void setupOptions();
+  }
+
   void connect();
   void disconnect();
   void disconnect(CassSession* connection); // Overload for connection unique_ptr
 
-  void setupSSL();
-  void configureOptions();
-
-  // Network connections options
-  constexpr const char* contactPoints = "db.buildinger.org";
-  constexpr int port = 6969;
-
-  // I would usually use a .env
-  constexpr const char* username = "bc-h9u0XvYWuDFmFUQr4mqH3mzpM16ujP";
-  constexpr const char* password = "h9u0XvYWuDFmFUQr4mqH3mzpM16ujPh9u0XvYWuDFmFUQr4mqH3mzpM16ujP";
+  /**
+   * Prepares a statement to be used for a query
+   */
+  void prepareStatement(const std::unique_ptr<const CassPrepared, void(*)(const CassPrepared*)>& statement, const std::string& query);
 
   inline std::unique_ptr<CassSession, void(*)(CassSession*)> connection(cass_session_new(), Database::disconnect);
   inline std::unique_ptr<CassCluster, void(*)(CassCluster*)> cluster(cass_cluster_new(), cass_cluster_free);
