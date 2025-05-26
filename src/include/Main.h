665dd08c81731284984dbb2d9e6e5497ccbec649
Shayden seymour <shaydenseymour@gmail.com>
2025-05-27 09:14:43 +1200

    Error occurred string to be used in returnErrorNotification

--- src/include/Main.h
+++ src/include/Main.h
@@ -1,3 +1,6 @@
 #pragma once
 
 void shutdown(const int& code) noexcept;
+
+// Used on returnErrorNotification
+constexpr const char* ERROR_OCCURRED = "An internal server error occurred, try again later.";
